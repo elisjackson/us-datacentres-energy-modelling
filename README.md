95d36a0126f1de8e24b4455c0c5791055a441cc6
Elis Jackson <ewbjackson@gmail.com>
2025-11-16 16:03:30 +0000

    update readme and add docs file

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
 # About
 
-# Installation
+A simple Streamlit dashboard and energy model for off-grid US datacentres.
 
-`pip -m venv .venv`
-`pip install -r requirements.txt`
+Visit my GitHub pages: [https://elisjackson.github.io/](elisjackson.github.io)
 
-# Data preparation
+# Installation (using uv)
 
-1. `generate_hex_cell_data\wind_and_pv_data.py` generates wind speed and PV GTI data, and outputs a hex cell representation (geojson file) of the US
+`uv venv`
+`uv sync`
 
-# Streamlit app
+# Run the dashboard (Streamlit app)
 
-`.venv\Scripts\activate`
-`streamlit run streamlit_dashboard.py`
+`uv run streamlit run src/streamlit_dashboard.py`
+
+# Energy model
+
+The Streamlit app calls the `src/energy_model` module, using front end inputs. The module can also be executed stand-alone. To execute:
+`uv run streamlit run src/energy_model/calculate_energy.py`
+
+# Hex cell generator
+
+The `src/generate_hex_cell_data` module downloads and processes US wind speed and Global Tilted Irradiance (GTI) data, and outputs two JSON files (to `src/hex_cell_data/`), that is used by the Streamlit app. It is not required to execute this module if you have cloned the repository as the files have been pre-generetaed.
+
+The files saved to `src/hex_cell_data/` include:
+- (GEOJSON) US wind speed and GTI data, with the US dividied into hex cells
+- (JSON) A lookup US states -> hex cell IDs
+
+To execute:
+`uv run streamlit run src/generate_hex_cell_data/generate_data.py`
+
+# Docs and assumptions
+
+For documentation and assumptions, see the `docs` folder.
--- src/streamlit_dashboard.py
+++ src/streamlit_dashboard.py
@@ -632,53 +632,5 @@ with st.expander("2️⃣ Select capacities", expanded=True):
     select_capacities_expander()
 
 with st.expander("Methodology and assumptions"):
-    st.markdown(
-        """
-        ### Map data
-        - **Wind speed data**:
-        National Renewable Energy Laboratory -
-        Wind Integration National Dataset (WIND) Toolkit, Multi-year Annual Average
-
-        - **Solar irradiance data**:
-        [globalsolaratlas.info](https://globalsolaratlas.info/) Global Solar Atlas 2.0 -
-        Longterm yearly average of global irradiation at optimum tilt
-
-        ### Energy model
-        A simple energy model is used to calculate the energy supply and demand for
-        the off-grid data centre, given the user's inputs for renewable capacities and battery storage.
-        On-site gas CCGT generation is assumed to kick in when no renewable generation is available.\n
-
-        - **Data centre demand**:
-          - Data centre demand is assumed to be constant at the selected capacity
-            - In reality there will be variation e.g. seasonally with ambient temperature (affecting cooling demands),
-            and depending on server workload.
-        - **Wind generation**:
-          - **Weather file:** Typical Meteorological Year (TMY) weather from [NREL](https://nsrdb.nrel.gov/data-viewer) -
-          USA & Americas - Typical Meteorological Year - tmy-2024
-          - Wind speed data is scaled to match the annual mean wind speed (from "Map data") at the selected hex cell
-          - Power calculated using Vestas V164-8.0 power curve
-        - **PV generation**:
-          - [pvlib-python](https://pvlib-python.readthedocs.io/en/v0.11.2/user_guide/weather_data.html)
-        model used to calculate Solar PV generation
-          - There is no link between the solar irradiance data from "Map data" and the PV model.
-          pv-lib uses it's own TMY APIs to fetch data for the location.
-        - **BESS generation**: Simple BESS model is used with:
-          - Round trip efficiency: 95%
-          - Charges only from renewable generation
-          - BESS can charge to 100% State of Charge (SOC) and discharge to 0% SOC
-        - **Gas generation (CCGT)**:
-          - Gas fills the gap after wind, PV and BESS generation
-        has been subtracted from demand.
-          - CCGT efficiency assumed: 50%
-          - CCGT is assumed to be able to provide generation at a moment's notice
-          (i.e. zero cold start time). In reality 30-60 minutes may be required for a cold start.
-          - Similarly, CCGT is assumed to be able to shutdown instantly.
-          - CCGT capacity required is the maximum demand over the year
-          (no additional capacity margin is assumed).
-        - **Gas fuel and CAPEX, OPEX cost assumptions for CCGT, Solar PV, BESS, Wind**:
-        Sourced from [GNESTE](https://github.com/iain-staffell/GNESTE),
-        averaged across all 2020-2025 values for each technology / cost type
-        (for quick development)
-        - **Gas emissions assumed**: 202 kgCO2 per MWh of gas consumption
-        """
-    )
+    docs_path = Path("docs/method_and_assumptions.md")
+    st.markdown(docs_path.read_text(), unsafe_allow_html=True)
